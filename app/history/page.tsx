4c79d8b9303f3d53a9390645162bef98a589ca0c
azhis fauzan prasetiyo <azhisfauzan209@gmail.com>
2025-12-06 08:42:47 +0700

    tambahan history

---
+++ app/history/page.tsx
@@ -0,0 +1,9 @@
+export default function HistoryPage() {
+    return (
+      <div className="p-10 text-center">
+        <h1 className="text-2xl font-bold">Riwayat Diagnosa</h1>
+        <p>Halaman riwayat belum dibuat.</p>
+      </div>
+    );
+  }
+  
